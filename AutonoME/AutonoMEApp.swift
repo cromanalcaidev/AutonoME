4232a36899a1d4d5ad1ab26c685a0fea8488a5de
Carlos Román Alcaide <cromanalcaidev@gmail.com>
2024-04-06 11:18:14 +0200

    Initial Commit

---
+++ AutonoME/AutonoMEApp.swift
@@ -0,0 +1,17 @@
+//
+//  AutonoMEApp.swift
+//  AutonoME
+//
+//  Created by Carlos Román Alcaide on 6/4/24.
+//
+
+import SwiftUI
+
+@main
+struct AutonoMEApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
